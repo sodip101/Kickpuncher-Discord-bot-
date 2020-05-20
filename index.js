04fa2e5948e8b39fc43161a1bebbdf917a1794bb
Sodip Bikram Thapa <sodip99@gmail.com>
2020-05-20 07:30:46 +0545

    added youtube video search feature

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+//YouTube Video Search package
+const yts=require('yt-search');
 //Import Config file
-const config=require('./config.json')
+const {prefix,token,sourcecode}=require('./config.json')
 //Import Discord.js module
 const Discord=require('discord.js');
 //Creating new Discord client
@@ -12,23 +14,43 @@ client.once('ready', ()=>{
 
 //Listening for messages
 client.on('message', message=>{
-    let message_data=message.content;
-    if(message_data ==='Kickpuncher'){
+    if(message.content.toLowerCase() ==='kickpuncher'){
         message.reply("Don't call me Kickpuncher. Call me, David.");
-    }
-    else if(message_data.startsWith(config.prefix+"kickpunch")){
-        if(message.member.hasPermission('ADMINISTRATOR')){
-            let member=message.mentions.members.first();
-            member.kick().then((member)=>{
-                message.channel.send(member.displayName+" has been kickpunched!");
-        });
-        }else{
-            message.reply("You are not powerful enough to do that, loser.");
+    }else{
+        const args = message.content.slice(prefix.length).split(/ +/);
+        const command = args.shift().toLowerCase();
+
+        switch (command) {
+            case 'sourcecode':
+                message.reply(sourcecode)
+                break;
+            case 'yts':
+                if(!args) message.reply('Please provide a search term.');
+                let searchterm=args.join(" ");
+                yts( searchterm, function ( err, r ) {
+                    if(err){
+                        console.log(err);
+                    }
+                    const video = r.videos[0].url;
+                    message.channel.send(video);
+                });
+                break;
+            case 'kickpunch':
+                if(!message.mentions.users.size){
+                    message.reply('Please mention the member who you want to get Kickpunched.');
+                }
+                if(message.member.hasPermission('ADMINISTRATOR')){
+                    let member=message.mentions.members.first();
+                    member.kick().then((member)=>{
+                        message.channel.send(member.displayName+" has been kickpunched!");
+                    });
+                }else{
+                    message.reply("You are not powerful enough to do that, loser.");
+                }
+                break;
         }
-    }else if(message_data.startsWith(config.prefix+"sourcecode")){
-        message.reply('https://github.com/sodip101/Kickpuncher-Discord-bot-');
     }
 });
 
 //Logging in to Discord using app token 
-client.login(config.token);
+client.login(token);
