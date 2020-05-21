48691c7b689fb2c3288bfcf7080ddf4424a9c7f7
Sodip Bikram Thapa <sodip99@gmail.com>
2020-05-21 09:01:15 +0545

    added command for setting nickname

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //YouTube Video Search package
 const yts=require('yt-search');
+//https module
+const https=require('https');
 //Import Config file
 const {prefix,token,sourcecode}=require('./config.json')
 //Import Discord.js module
@@ -8,23 +10,25 @@ const Discord=require('discord.js');
 const client=new Discord.Client();
 
 //'ready' event is triggered after login is succesful
-client.once('ready', ()=>{
+client.once('ready', (err)=>{
+    if (err) console.log(err);
     console.log('Ready!');
 });
 
 //Listening for messages
-client.on('message', message=>{
+client.on('message', function(message){
     if(message.content.toLowerCase() ==='kickpuncher'){
         message.reply("Don't call me Kickpuncher. Call me, David.");
     }else{
+        //store user message in array after removing prefix and spliting between spaces using regex
         const args = message.content.slice(prefix.length).split(/ +/);
-        const command = args.shift().toLowerCase();
+        const command = args.shift().toLowerCase();//remove command from array and change to lowercase
 
         switch (command) {
-            case 'sourcecode':
+            case 'sourcecode'://display github repo link
                 message.reply(sourcecode)
                 break;
-            case 'yts':
+            case 'yts'://return first link of youtube video search
                 if(!args) message.reply('Please provide a search term.');
                 let searchterm=args.join(" ");
                 yts( searchterm, function ( err, r ) {
@@ -35,8 +39,8 @@ client.on('message', message=>{
                     message.channel.send(video);
                 });
                 break;
-            case 'kickpunch':
-                if(!message.mentions.users.size){
+            case 'kickpunch'://kick member
+                if(!message.mentions.members.size){
                     message.reply('Please mention the member who you want to get Kickpunched.');
                 }
                 if(message.member.hasPermission('ADMINISTRATOR')){
@@ -46,6 +50,24 @@ client.on('message', message=>{
                     });
                 }else{
                     message.reply("You are not powerful enough to do that, loser.");
+                }
+                break;
+            case 'call'://set member nickname
+                if(!message.mentions.members.size){
+                    message.reply('Please mention the member whose nickname you want to change.');
+                }else if(message.mentions.members.size){
+                    if(!args[1]){
+                        message.reply('Please provide a nickname.');
+                    }else if(args[1]){
+                        if(message.member.hasPermission('ADMINISTRATOR')){
+                            let member=message.mentions.members.first();
+                            member.setNickname(args[1]).then((member)=>{
+                                message.channel.send(member.user.tag+" will now be called "+ args[1]);
+                            });
+                        }else if(!message.member.hasPermission('ADMINISTRATOR')){
+                            message.reply("You're not powerful enough to do that, loser.");
+                        }
+                    }
                 }
                 break;
         }
